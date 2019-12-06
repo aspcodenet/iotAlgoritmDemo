28719be755b4a47809f13253caa519a3bef6c77f
Stefan Holmberg <stefan.holmberg@systementor.se>
2019-12-06 12:57:40 +0100

    a

--- IotAlgoritmDemo/IotAlgoritmDemo.cpp
+++ IotAlgoritmDemo/IotAlgoritmDemo.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <list>
 
 using namespace std;
 
@@ -14,21 +15,105 @@ enum class Position
 
 class Player
 {
-	string name;
+	string fornamn;
+	string efternamn;
 	Position position;
+	int salary;
 public:
 	Position GetPosition() { return position; }
-	string GetName() { return name; }
-	Player(string name, Position position)
+	string GetFornamn() { return fornamn; }
+	string GetEfternamn() { return efternamn; }
+	int GetSalary() { return salary; }
+	void SetNewSalary(int s) { salary = s; }
+	Player(string fornamn, string efternamn, Position position, int salary)
 	{
-		this->name = name;
+		this->salary = salary;
+		this->fornamn = fornamn;
+		this->efternamn = efternamn;
 		this->position = position;
 	}
 };
 
 
 
+
 int main()
 {
+	vector<int> talLista = { 1,323,545,55,45,213,55, 231231,231 };
+	
+	auto result = minmax_element(begin(talLista), end(talLista));
+
+
+	replace(talLista.begin(), talLista.end(), 55, 0);
+	//replace(begin(talLista), end(talLista), 55, 0);
+
+	//sort(talLista.begin(), talLista.end(), std::greater<int>());
+	reverse(talLista.begin(),talLista.end());
+
+	for (auto i : talLista)
+	{
+		cout << i << endl;
+	}
+
+
+
+	vector<Player> players = {
+		Player("William", "Karlsson", Position::Forward,5000),
+		Player("Mats", "Sundin", Position::Forward,4000),
+		Player("Masken", "Karlsson", Position::Forward,3000),
+		Player("Peter","Forsberg",  Position::Forward,3000),
+		Player("Niklas", "Lidström", Position::Defence,4000)
+	};
+
+	int antal = count_if(begin(players), end(players), [](Player p) {
+		if (p.GetPosition() == Position::Forward && p.GetSalary() > 4500) return true;
+		return false;
+		});
+
+	bool foundGoalie = all_of(players.begin(), players.end(), [](Player& p) {
+		if (p.GetPosition() == Position::Goalie) return true;
+		return false;
+		//return p.GetPosition() == Position::Goalie;
+		});
+
+
+	for_each(players.begin(), players.end(), [](Player& p) {
+		p.SetNewSalary(p.GetSalary() + 10);
+		});
+
+	for (Player &p : players)
+	{
+		p.SetNewSalary(p.GetSalary() + 10);
+	}
+
+	for (auto pl : players)
+	{
+		cout << pl.GetFornamn() << " " << pl.GetSalary() << endl;
+	}
+
+
+
+	sort(players.begin(), players.end(), [](Player p1, Player p2) 
+				{
+					if (p1.GetEfternamn() == p2.GetEfternamn())
+						return p1.GetFornamn() < p2.GetFornamn();
+					return p1.GetEfternamn() < p2.GetEfternamn();
+				}
+	);
+	for (auto pl : players)
+	{
+		cout << pl.GetFornamn() << endl;
+	}
+
+
+
+
+
+
+
+
+
+
+
     std::cout << "Hello World!\n";
 }
